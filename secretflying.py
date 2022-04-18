ac6993a1b226f9e4eaa8edc4bfe1002a614c408d
faisal.mirza <faisal.mirza@bellmedia.ca>
2022-04-17 23:12:54 -0400

    regex to search for Toronto

--- secretflying.py
+++ secretflying.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # secret flying's canadian deals
 source = requests.get('https://www.secretflying.com/canada-deals/').text
@@ -9,5 +10,17 @@ soup = BeautifulSoup(source, 'lxml')
 
 posts = soup.find_all('div', {'class':'article-content-wrapper entry-main-content'})
 
+def search_pattern(pattern, string):
+  result = re.search(pattern, string)
+  return result
+
+to_pattern = 'Toronto'
+
+deal_list =[]
+
 for post in posts:
-    print(post.h2.text)
+    if search_pattern(to_pattern, post.h2.text):
+        # deal_list.append(post.h2.text)
+        print(post.h2.text)
+
+# print(deal_list)
